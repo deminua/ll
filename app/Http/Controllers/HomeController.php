d1fc862cff58bd2a5ce37d70aeed03c4f8452482
deminua <dataranenko@gmail.com.ua>
2017-05-18 10:10:37 +0300

    upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //$this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   
+
+        #$exists = Storage::disk('s3')->exists('avatars/ArcheAge_sample.jpg');
+
+        #$files = Storage::disk('s3')->allFiles();
+        #return dd($files);
+
+        #$getVisibility = Storage::disk('s3')->getVisibility('avatars/ArcheAge_sample.jpg');
+        #return dd($getVisibility);
+
+        #$get = Storage::disk('s3')->get('avatars/ArcheAge_sample.jpg');
+        #return dd($get);
+
+
+        $url = Storage::disk('s3')->url('avatars/AM113_001_platanus_specular.jpg');
+        #return dd($url);
+        return response()->file($url);
+
+
+        $data = Storage::get('AM113_001_platanus_specular.jpg');
+                Storage::disk('s3')->put('avatars/AM113_001_platanus_specular.jpg', $data);
+
+
+        $files = Storage::disk('s3')->allFiles();
+        return dd($files);
+
+        return view('home');
+
+    }
+}
